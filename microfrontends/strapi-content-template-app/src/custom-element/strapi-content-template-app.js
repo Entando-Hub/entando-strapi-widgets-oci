83bdabe39803e4fb6895e0cce600c86b1dbe9a36
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2022-09-13 09:19:09 +0530

    Strapi-Content-Template-App Naming Pattern Changes

--- microfrontends/strapi-content-template-app/src/custom-element/strapi-content-template-app.js
+++ microfrontends/strapi-content-template-app/src/custom-element/strapi-content-template-app.js
@@ -4,35 +4,46 @@ import App from '../App'
 import { KEYCLOAK_EVENT_TYPE, subscribeToWidgetEvent } from "../helpers/widgetEvents"
 import { STRAPI_BASE_URL_KEY } from "../constant/constant"
 import { checkIfUrlExists, getStrapiConfigurations } from "../integration/StrapiAPI"
+
 const getKeycloakInstance = () =>
     (window && window.entando && window.entando.keycloak && { ...window.entando.keycloak, initialized: true }) || {
         initialized: false,
     }
-class EtApp extends HTMLElement {
+
+class StrapiContentTemplateApp extends HTMLElement {
+
     constructor() {
         super();
         this.reactRootRef = React.createRef();
         this.mountPoint = null;
     }
+
     #config = {
         locale: 'en',
     }
+
+    get config() {
+        return (this.reactRootRef && this.reactRootRef.current) ? this.reactRootRef.current.state : {};
+    }
+
+    set config(value) {
+        return (this.reactRootRef && this.reactRootRef.current) ? this.reactRootRef.current.setState(value) : {};
+    }
+
     #updateConfig(value) {
         this.#config = JSON.parse(value)
     }
+    
     static get observedAttributes() {
         return ["config"]
     }
+
     attributeChangedCallback(name, oldValue, newValue) {
         this.#updateConfig(newValue);
         this.getStrapiConfiguration(false);
     }
-    get config() {
-        return (this.reactRootRef && this.reactRootRef.current) ? this.reactRootRef.current.state : {};
-    }
-    set config(value) {
-        return (this.reactRootRef && this.reactRootRef.current) ? this.reactRootRef.current.setState(value) : {};
-    }
+
+
     connectedCallback() {
         this.mountPoint = document.createElement('span')
         this.appendChild(this.mountPoint);
@@ -50,10 +61,10 @@ class EtApp extends HTMLElement {
      */
      getStrapiConfiguration = async (isConnectedCallback) => {
         localStorage.removeItem(STRAPI_BASE_URL_KEY);
-        const configServerURLGet = this.#config.systemParams.api['ext-api']['url']
-        const { data, isError } = await getStrapiConfigurations(configServerURLGet); // TODO: Config need(local1337)
+        const configServerURLGet = this.#config && this.#config.systemParams && this.#config.systemParams.api['ext-api'] && this.#config.systemParams.api['ext-api']['url']
+        const { data, isError } = await getStrapiConfigurations(configServerURLGet);
         if (!isError && data && data.data && data.data.baseUrl) {
-            const result = await checkIfUrlExists(data.data.baseUrl); // TODO: Validating(local1337)
+            const result = await checkIfUrlExists(data.data.baseUrl);
             if (result && result.data && result.data.status === 200 && !result.isError) {
                 localStorage.setItem(STRAPI_BASE_URL_KEY, data.data.baseUrl);
             }
@@ -65,4 +76,5 @@ class EtApp extends HTMLElement {
         }
     }
 }
-customElements.get('et-strapi-template-app') || customElements.define("et-strapi-template-app", EtApp)
+
+customElements.get('strapi-content-template-app') || customElements.define("strapi-content-template-app", StrapiContentTemplateApp)
